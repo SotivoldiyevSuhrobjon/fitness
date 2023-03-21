ca6faf9d1c101be5074f01a2da6ea5348c4b176f
https://github.com/SotivoldiyevSuhrobjon <suhrobjondon@gmail.com>
2023-03-21 16:58:21 +0500

    fitness

---
+++ static/js/app.js
@@ -0,0 +1,39 @@
+window.onload = () => {
+    let loader = document.querySelector("#preloader");
+    loader.style.opacity = 0;
+    loader.style.visibility = "hidden";
+};
+
+
+
+let like = document.querySelector('.heart');
+let title_text = document.querySelector('#title_text').textContent;
+like.addEventListener('click', (e) => {
+    let prog_slug = e.target.dataset.favorit
+    let like_classes = e.target.classList
+    if (like_classes.contains('fa-regular') ) {
+        like.classList.remove('fa-regular')
+        like.classList.add('fa-solid');
+        ajaxLike(prog_slug, true, title_text)
+    } else {
+        like.classList.remove('fa-solid')
+        like.classList.add('fa-regular')
+        ajaxLike(prog_slug, false, title_text)
+    }
+});
+
+function removeFavoriteObj(e){
+    ajaxLike(e.target.dataset.favorite, false, 'test')
+    window.location.reload()
+}
+
+function ajaxLike(slug, is_liked, title){
+    $.ajax({
+        url: '/check_like/',
+        type: "GET",
+        data: { slug: slug, like: is_liked, title: title },
+        success: function(data){
+            console.log(data);
+        }
+    });
+}
